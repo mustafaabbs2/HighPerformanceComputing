0950f163e9476380e54419d87d441a40064f845e
Mustafa Bhotvawala <mustafa.bhotvawala@ansys.com>
2024-01-03 11:30:25 +0530

    changes

--- SharedMemory/GraphOptimization.cpp
+++ SharedMemory/GraphOptimization.cpp
@@ -137,3 +137,69 @@ void step_v3(float* r, const float* d_, int n)
 		}
 	}
 }
+
+#ifdef __GNUC__
+typedef float float8_t __attribute__((vector_size(8 * sizeof(float))));
+
+constexpr float8_t f8infty = {infty, infty, infty, infty, infty, infty, infty, infty};
+
+static inline float hmin8(float8_t vv)
+{
+	float v = infty;
+	for(int i = 0; i < 8; ++i)
+	{
+		v = std::min(vv[i], v);
+	}
+	return v;
+}
+
+static inline float8_t min8(float8_t x, float8_t y)
+{
+	return x < y ? x : y;
+}
+
+void step_v4(float* r, const float* d_, int n)
+{
+	// elements per vector
+	constexpr int nb = 8;
+	// vectors per input row
+	int na = (n + nb - 1) / nb;
+
+	// input data, padded, converted to vectors
+	std::vector<float8_t> vd(n * na);
+	// input data, transposed, padded, converted to vectors
+	std::vector<float8_t> vt(n * na);
+
+#	pragma omp parallel for
+	for(int j = 0; j < n; ++j)
+	{
+		for(int ka = 0; ka < na; ++ka)
+		{
+			for(int kb = 0; kb < nb; ++kb)
+			{
+				int i = ka * nb + kb;
+				vd[na * j + ka][kb] = i < n ? d_[n * j + i] : infty;
+				vt[na * j + ka][kb] = i < n ? d_[n * i + j] : infty;
+			}
+		}
+	}
+
+#	pragma omp parallel for
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = 0; j < n; ++j)
+		{
+			float8_t vv = f8infty;
+			for(int ka = 0; ka < na; ++ka)
+			{
+				float8_t x = vd[na * i + ka];
+				float8_t y = vt[na * j + ka];
+				float8_t z = x + y;
+				vv = min8(vv, z);
+			}
+			r[n * i + j] = hmin8(vv);
+		}
+	}
+}
+
+#endif
