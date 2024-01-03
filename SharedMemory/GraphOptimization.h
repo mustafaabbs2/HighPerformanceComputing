57b8dd496b019bc3e4a97af5091e090c46a1360d
Mustafa Bhotvawala <mustafa.bhotvawala@ansys.com>
2024-01-03 10:22:21 +0530

    changes

---
+++ SharedMemory/GraphOptimization.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void step_v0(float* r, const float* d, int n);
---
+++ SharedMemory/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "GraphOptimization.h"
+
+int main()
+{
+	constexpr int n = 3;
+	const float d[n * n] = {
+		0,
+		8,
+		2,
+		1,
+		0,
+		9,
+		4,
+		5,
+		0,
+	};
+
+	float r[n * n];
+
+	step_v0(r, d, n);
+
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = 0; j < n; ++j)
+		{
+			std::cout << r[i * n + j] << " ";
+		}
+		std::cout << "\n";
+	}
+}
