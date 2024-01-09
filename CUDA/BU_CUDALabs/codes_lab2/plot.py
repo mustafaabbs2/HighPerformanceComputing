ff6bfe286b1bd2231f00fe60a807cc7df0de41fd
Mustafa Bhotvawala <mustafa.bhotvawala@ansys.com>
2024-01-09 18:01:47 +0530

    removed BULabs

--- CUDA/BU_CUDALabs/codes_lab2/plot.py
+++
@@ -1,9 +0,0 @@
-
-with open('temperature_cpu.txt', 'r') as fp:
-    data = [list(line.strip().split('\t')) for line in fp]
-
-data.remove([''])
-
-print([elem[0] for elem in data])
-# print([elem[1] for elem in data])
-# print([elem[2] for elem in data])
